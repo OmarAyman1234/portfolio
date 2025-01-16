e01140e77f8a100466b92c28271d861a9e00ba4d
OmarAyman1234 <omarayman9124@gmail.com>
2025-01-16 23:10:11 +0200

    About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import About from "./Components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import { createContext, useRef } from "react";
 
+export const ScrollContext = createContext({});
 function App() {
+  const aboutRef = useRef(null);
+  const skillsRef = useRef(null);
+  const projectsRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const ScrollValues = {
+    aboutRef,
+    skillsRef,
+    projectsRef,
+    contactRef,
+  };
   return (
     <>
-      <Header />
-      <Hero />
+      <ScrollContext.Provider value={ScrollValues}>
+        <Header />
+        <Hero />
+        <About />
+      </ScrollContext.Provider>
     </>
   );
 }
--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { ScrollContext } from "../../App";
 
 function Header() {
   const [activeLink, setActiveLink] = useState("");
   const [isVisible, setIsVisible] = useState(false); //modify height and opacity
   const [dropDown, setDropDown] = useState(false); //modify display
+  const { aboutRef, skillsRef, projectsRef, contactRef } =
+    useContext(ScrollContext);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      entry.isIntersecting ? setActiveLink("about") : setActiveLink("");
+    });
+    observer.observe(aboutRef.current);
+  }, []);
 
   const handleNavIconClick = (section) => {
     setActiveLink(section);
@@ -28,55 +38,55 @@ function Header() {
   return (
     <header className="fixed left-0 right-0 top-0 z-50 flex flex-col bg-gray-900 sm:flex-row sm:items-center sm:justify-between">
       <div className="flex h-20 items-center justify-between px-6 sm:px-10">
-        <p className="text-primary text-center text-xl font-extrabold xl:text-2xl">
+        <p className="text-center text-xl font-extrabold text-primary xl:text-2xl">
           Omar Ayman
         </p>
         <button
-          className={`menu-icon ${dropDown ? "text-white" : ""} hover:text-primary text-xl text-gray-300 sm:hidden`}
+          className={`menu-icon ${dropDown ? "text-white" : ""} text-xl text-gray-300 hover:text-primary sm:hidden`}
           onClick={handleNavMenuIcon}
         >
           <FontAwesomeIcon icon={faBars} />
         </button>
       </div>
       <div
-        className={`nav-dropdown ${dropDown ? "flex" : "hidden"} ${isVisible ? "h-9 opacity-100" : "h-0 opacity-0"} text-primary items-center justify-between px-6 text-xl transition-all duration-300 sm:flex sm:h-20 sm:pr-10 sm:text-2xl sm:opacity-100 xl:text-3xl`}
+        className={`nav-dropdown ${dropDown ? "flex" : "hidden"} ${isVisible ? "h-9 opacity-100" : "h-0 opacity-0"} items-center justify-between px-6 text-xl text-primary transition-all duration-300 sm:flex sm:h-20 sm:pr-10 sm:text-2xl sm:opacity-100 xl:text-3xl`}
       >
-        <a
-          href="#about"
+        <button
           className={`nav-link ${activeLink === "about" ? "active text-white" : ""}`}
           onClick={() => {
+            aboutRef.current?.scrollIntoView({
+              behavior: "smooth",
+              block: "start",
+            });
             handleNavIconClick("about");
           }}
         >
           About
-        </a>
-        <a
-          href="#skills"
+        </button>
+        <button
           className={`nav-link ${activeLink === "skills" ? "active text-white" : ""}`}
           onClick={() => {
             handleNavIconClick("skills");
           }}
         >
           Skills
-        </a>
-        <a
-          href="#projects"
+        </button>
+        <button
           className={`nav-link ${activeLink === "projects" ? "active text-white" : ""}`}
           onClick={() => {
             handleNavIconClick("projects");
           }}
         >
           Projects
-        </a>
-        <a
-          href="#contact"
+        </button>
+        <button
           className={`nav-link ${activeLink === "contact" ? "active text-white" : ""}`}
           onClick={() => {
             handleNavIconClick("contact");
           }}
         >
           Contact
-        </a>
+        </button>
       </div>
     </header>
   );
