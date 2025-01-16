5d3213e750936179d54bde6e8dcd31bcdd58222a
OmarAyman1234 <omarayman9124@gmail.com>
2025-01-16 16:49:45 +0200

    Fixed the navbar problem
    Navbar icons were clickable when the dropdown was closed.

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,33 +1,45 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   const [activeLink, setActiveLink] = useState("");
+  const [isVisible, setIsVisible] = useState(false); //modify height and opacity
+  const [dropDown, setDropDown] = useState(false); //modify display
 
   const handleNavIconClick = (section) => {
     setActiveLink(section);
   };
 
-  const [dropDown, setDropDown] = useState(false);
-
   const handleNavMenuIcon = () => {
-    setDropDown(!dropDown);
+    if (dropDown) {
+      setIsVisible(false);
+      setTimeout(() => {
+        setDropDown(false);
+      }, 300);
+    } else {
+      setDropDown(true);
+      setTimeout(() => {
+        setIsVisible(true);
+      }, 50);
+    }
   };
 
   return (
     <header className="fixed left-0 right-0 top-0 z-50 flex flex-col bg-gray-900 sm:flex-row sm:items-center sm:justify-between">
       <div className="flex h-20 items-center justify-between px-6 sm:px-10">
-        <p className="text-center text-xl font-extrabold text-gray-100 xl:text-2xl">
+        <p className="text-primary text-center text-xl font-extrabold xl:text-2xl">
           Omar Ayman
         </p>
         <button
-          className={`menu-icon ${dropDown ? "text-white" : ""} text-xl text-gray-300 hover:text-white sm:hidden`}
+          className={`menu-icon ${dropDown ? "text-white" : ""} hover:text-primary text-xl text-gray-300 sm:hidden`}
           onClick={handleNavMenuIcon}
         >
-          &#9776;
+          <FontAwesomeIcon icon={faBars} />
         </button>
       </div>
       <div
-        className={`nav-dropdown ${dropDown ? "h-9 opacity-100" : "h-0 opacity-0"} flex items-center justify-between px-6 text-xl text-gray-100 transition-all duration-300 sm:h-20 sm:pr-10 sm:text-2xl sm:opacity-100 xl:text-3xl`}
+        className={`nav-dropdown ${dropDown ? "flex" : "hidden"} ${isVisible ? "h-9 opacity-100" : "h-0 opacity-0"} text-primary items-center justify-between px-6 text-xl transition-all duration-300 sm:flex sm:h-20 sm:pr-10 sm:text-2xl sm:opacity-100 xl:text-3xl`}
       >
         <a
           href="#about"
