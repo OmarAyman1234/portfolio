44c10be7c0788a65ddf5f685c06710c192af96a6
OmarAyman1234 <omarayman9124@gmail.com>
2025-01-17 18:01:46 +0200

    Projects data

---
+++ src/Data/projects.js
@@ -0,0 +1,81 @@
+export const projects = {
+  Featured: [
+    {
+      previewImg: "/src/assets/images/shopme-preview.png",
+      name: "SHOPME",
+      description:
+        "An ecommerce website fully developed by me that has lots of features including but not limited to adding products to favorites, cart, and viewing orders history, all data is saved to the local storage.",
+      //HTML, CSS, JS
+      includedSkills: [
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+        "/src/assets/icons/Official_CSS_Logo.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      ],
+      sourceCode: "https://github.com/OmarAyman1234/SHOPME",
+      liveDemo: "https://omarayman1234.github.io/SHOPME/",
+    },
+    {
+      previewImg: "/src/assets/images/portfolio-preview.png",
+      name: "Portfolio",
+      description:
+        "My portfolio website that you are currently viewing, there is no need for a description 😄.",
+      //React, Tailwind
+      includedSkills: [
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+      ],
+      sourceCode: "https://github.com/OmarAyman1234/portfolio",
+      liveDemo: "https://omarayman1234.github.io/portfolio/",
+    },
+    {
+      previewImg: "/src/assets/images/maktabaHub-preview.png",
+      name: "Maktaba Hub",
+      description:
+        "An islamic library for borrowing and reserving books, I made the data file, and worked on rendering book details and borrow/reserve pages using the data file and search params.",
+      includedSkills: [
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+        "/src/assets/icons/Official_CSS_Logo.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      ],
+      sourceCode: "https://github.com/OmarAyman1234/BookStore",
+      liveDemo: "https://omarayman1234.github.io/BookStore/",
+    },
+  ],
+
+  Other: [
+    {
+      previewImg: "/src/assets/images/cairoPhones-preview.png",
+      name: "Cairo Phones",
+      description:
+        "My first project, I worked on the Samsung page, and supervised the whole project.",
+      //HTML, CSS, JS
+      includedSkills: [
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+        "/src/assets/icons/Official_CSS_Logo.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      ],
+      sourceCode: "https://github.com/OmarAyman1234/CairoPhones.github.io",
+      liveDemo: "https://omarayman1234.github.io/CairoPhones.github.io/",
+    },
+    {
+      previewImg: "/src/assets/images/health-club-preview.png",
+      name: "Health Club Management System",
+      description:
+        "A Gym system built using Java, I worked on the text files structure, notification system, billing system, and some functions in the FileHandler.java file",
+      includedSkills: [
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg",
+      ],
+      sourceCode:
+        "https://github.com/OmarAyman1234/health-club-management-system",
+      liveDemo: "",
+    },
+  ],
+  // {
+  //   previewImg: "",
+  //   name: "",
+  //   description: "",
+  //   includedSkills: [],
+  //   sourceCode: "",
+  //   liveDemo: "",
+  // },
+};
