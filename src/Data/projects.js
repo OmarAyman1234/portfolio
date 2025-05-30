650f13a3ff8235fd6c7718bae0a14203be45cb95
OmarAyman1234 <omarayman9124@gmail.com>
2025-05-30 23:24:28 +0300

    Added Multimedia project

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,6 +1,33 @@
 export const projects = {
   Featured: [
     {
+      previewImg: "/assets/images/multimedia-preview.png",
+      name: "Multimedia",
+      description:
+        "A Wikipedia-like college project <strong>led by me</strong>. I contributed primarily to the <strong>development;</strong> building key features like the <strong>article page</strong>, <strong>profile page</strong>, <strong>article reports</strong>, and the <strong>system PDF report</strong>. <br><br> I also designed the <strong>ERD diagram</strong> for the database.",
+      includedSkills: [
+        {
+          name: "PHP",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg",
+        },
+        {
+          name: "MySQL",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg",
+        },
+        {
+          name: "UML",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/unifiedmodelinglanguage/unifiedmodelinglanguage-original.svg",
+        },
+        {
+          name: "Bootstrap",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg",
+          borderColor: "#7311f6",
+        },
+      ],
+      sourceCode: "https://github.com/OmarAyman1234/multimedia",
+      demo: "https://drive.google.com/file/d/17NpKZ1NQZ_7g1JkxJmt3AbyTmRltvv5v/view",
+    },
+    {
       previewImg: "/assets/images/todoList-preview.png",
       name: "ToDoList",
       description:
@@ -26,6 +53,9 @@ export const projects = {
       sourceCode: "https://github.com/OmarAyman1234/todo-list",
       demo: "https://oa-todo.vercel.app",
     },
+  ],
+
+  Other: [
     {
       previewImg: "/assets/images/shopme-preview.png",
       name: "SHOPME",
@@ -45,9 +75,6 @@ export const projects = {
       sourceCode: "https://github.com/OmarAyman1234/SHOPME",
       demo: "https://omarayman1234.github.io/SHOPME/",
     },
-  ],
-
-  Other: [
     {
       previewImg: "/assets/images/maktabaHub-preview.png",
       name: "Maktaba Hub",
@@ -67,25 +94,25 @@ export const projects = {
       sourceCode: "https://github.com/OmarAyman1234/BookStore",
       demo: "https://omarayman1234.github.io/BookStore/",
     },
-    {
-      previewImg: "/assets/images/cairoPhones-preview.png",
-      name: "Cairo Phones",
-      description:
-        "My first project, I worked on the Samsung page, and supervised the whole project.",
-      includedSkills: [
-        {
-          name: "HTML",
-          img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
-        },
-        { name: "CSS", img: "/assets/icons/Official_CSS_Logo.svg" },
-        {
-          name: "JavaScript",
-          img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-        },
-      ],
-      sourceCode: "https://github.com/OmarAyman1234/CairoPhones.github.io",
-      demo: "https://omarayman1234.github.io/CairoPhones.github.io/",
-    },
+    // {
+    //   previewImg: "/assets/images/cairoPhones-preview.png",
+    //   name: "Cairo Phones",
+    //   description:
+    //     "My first project, I worked on the Samsung page, and supervised the whole project.",
+    //   includedSkills: [
+    //     {
+    //       name: "HTML",
+    //       img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+    //     },
+    //     { name: "CSS", img: "/assets/icons/Official_CSS_Logo.svg" },
+    //     {
+    //       name: "JavaScript",
+    //       img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+    //     },
+    //   ],
+    //   sourceCode: "https://github.com/OmarAyman1234/CairoPhones.github.io",
+    //   demo: "https://omarayman1234.github.io/CairoPhones.github.io/",
+    // },
     {
       previewImg: "/assets/images/health-club-preview.png",
       name: "Health Club System",
