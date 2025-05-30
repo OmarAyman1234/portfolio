7a88067f9a37a16a9bd4518ec83e9873fce711a3
OmarAyman1234 <omarayman9124@gmail.com>
2025-05-30 21:05:30 +0300

    Skill hover improvement

--- src/Components/Skills/Skill.jsx
+++ src/Components/Skills/Skill.jsx
@@ -2,7 +2,7 @@ function Skill(props) {
   return (
     <div
       style={{ border: `3px solid ${props.borderColor}` }}
-      className="flex min-w-[105px] max-w-[105px] flex-col items-center justify-center rounded-md bg-gray-100 px-4 py-2 duration-150 hover:bg-gray-200 sm:min-w-[115px] sm:max-w-[115px] lg:min-w-[120px] xl:max-w-[120px]"
+      className="flex min-w-[105px] max-w-[105px] flex-col items-center justify-center rounded-md bg-gray-100 px-4 py-2 duration-150 hover:scale-105 hover:bg-slate-200 sm:min-w-[115px] sm:max-w-[115px] lg:min-w-[120px] xl:max-w-[120px]"
     >
       <img className="w-12" src={props.img} alt={props.name} />
       <p className="text-lg font-semibold">{props.name}</p>
